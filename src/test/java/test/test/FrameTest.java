c903e7e48ecafa43a222eb3a2158737db2b72a85
Cintia Galain <solgalain@gmail.com>
2017-11-27 16:34:54 -0300

    Primer commit

---
+++ src/test/java/test/test/FrameTest.java
@@ -0,0 +1,26 @@
+package test.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FrameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void intentoTest() {
+		Frame fr= new Frame();
+		fr.sumarPuntaje(3,5);
+		fr.sumarPuntaje(6,7);
+		assertEquals(21,fr.getPuntos());
+	}
+}
